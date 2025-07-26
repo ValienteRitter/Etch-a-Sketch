9e28ea251f7b776b59d8097d08fde26d75250afd
ValienteRitter <tagizade2006f@gmail.com>
2025-07-26 12:37:49 +0400

    Extend the available colors
    Users now have access to wider selection of colors

--- script.js
+++ script.js
@@ -2,7 +2,38 @@ const appContainer = document.querySelector('.app-container')
 const colorMenu = document.querySelector('.color-menu')
 const gridContainer = document.querySelector('.grid-container')
 const gridItems = []
-const colors = ['red', 'blue', 'green', 'yellow']
+const colors = [
+  "white",          // #FFFFFF
+  "black",          // #000000
+  "gray",           // #808080
+  "lightgray",      // #D3D3D3
+  "darkgray",       // #A9A9A9
+  "red",            // #FF0000
+  "darkred",        // #8B0000
+  "green",          // #008000
+  "lime",           // #00FF00
+  "darkgreen",      // #006400
+  "blue",           // #0000FF
+  "darkblue",       // #00008B
+  "lightblue",      // #ADD8E6
+  "navy",           // #000080
+  "teal",           // #008080
+  "aqua",           // #00FFFF
+  "cyan",           // #00FFFF
+  "purple",         // #800080
+  "violet",         // #EE82EE
+  "pink",           // #FFC0CB
+  "hotpink",        // #FF69B4
+  "orange",         // #FFA500
+  "darkorange",     // #FF8C00
+  "yellow",         // #FFFF00
+  "gold",           // #FFD700
+  "brown",          // #A52A2A
+  "beige",          // #F5F5DC
+  "coral",          // #FF7F50
+  "salmon",         // #FA8072
+  "transparent"     // special value
+];
 
 let currentColor
 
